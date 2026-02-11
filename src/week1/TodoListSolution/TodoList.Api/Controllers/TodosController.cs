e2041ae26dcae3a9abb3ad5742ff70d99d9756b2
Martin DeJarnette <mbdej1214@gmail.com>
2026-02-11 14:01:33 -0500

    copying jeff's code

--- src/week1/TodoListSolution/TodoList.Api/Controllers/TodosController.cs
+++ src/week1/TodoListSolution/TodoList.Api/Controllers/TodosController.cs
@@ -1,5 +1,19 @@
-﻿namespace TodoList.Api.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
 
-public class TodosController
+namespace TodoList.Api.Controllers;
+
+public class TodosController : ControllerBase
 {
+    // When they a get request /todos
+    [HttpGet("/todos")]
+    public async Task<ActionResult> GetAllTodos()
+    {
+        
+        return Ok(new List<string> {  "Clean The Garage", "Take Out The Trash"});
+    }
+    // GET /todos/{id}
+
+    // POST /
+
+    // PUT /todos/{id}/person
 }
