0d0805250b45571d6732d1a39ca477fd900a90a8
Martin DeJarnette <mbdej1214@gmail.com>
2026-02-12 12:42:24 -0500

    before lunch

---
+++ src/week1/BankingSolution/Banking.Domain/Account.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Banking.Domain;
+
+public class Account
+{
+    public Account()
+    {
+    }
+
+    public decimal GetBalance()
+    {
+        return 5000M; // fake. bs. slime.
+    }
+}
---
+++ src/week1/BankingSolution/Banking.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿namespace Banking.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CanAddTenAndTwenty()
+    {
+        // Arrange - Given
+        int a = 10;
+        int b = 20;
+        int answer;
+
+        // Act - When
+        answer = a + b;
+
+        // Assert - Then
+        Assert.Equal(30, answer);
+    }
+
+    [Theory]
+    [InlineData(10,20,30)]
+    [InlineData(2,2,4)]
+    [InlineData(3,3,6)]
+    public void CanAddOtherIntegersToo(int a, int b, int expected)
+    {
+        int answer = a + b;
+        Assert.Equal(expected, answer);
+    }
+}
