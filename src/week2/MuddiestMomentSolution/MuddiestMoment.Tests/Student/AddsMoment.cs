5d85253179ca2b99760d24972b8da1cb02a40956
Martin DeJarnette <mbdej1214@gmail.com>
2026-02-16 15:50:27 -0500

    commit

---
+++ src/week2/MuddiestMomentSolution/MuddiestMoment.Tests/Student/AddsMoment.cs
@@ -0,0 +1,37 @@
+﻿
+
+using Alba;
+using MuddiestMoment.Api.Student.Endpoints;
+using System.Reflection;
+
+namespace MuddiestMoment.Tests.Student;
+
+public class AddsMoment
+{
+    [Fact]
+    public async Task CanAddMoment()
+    {
+        var host = await AlbaHost.For<Program>();
+
+        // scenario
+        // start up the api
+        // make the request with some data to /student/moments
+        // that status code should be a 200
+        // we should also get some stuff back
+        // part 2 later
+
+        var itemToSend = new StudentMomentCreateModel
+        {
+            Title = "Containers",
+            Description = "Tell me about volumes"
+        };
+
+        var response = await host.Scenario(api =>
+        {
+            // Fluent interface - a "Domain Specific Langague"
+            api.Post.Json(itemToSend).ToUrl("/student/moments");
+            api.StatusCodeShouldBeOk();
+        });
+
+    }
+}
