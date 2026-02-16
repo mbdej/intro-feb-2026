9111460d63b6a3a11b1a2d2212e02e52ec763d52
Martin DeJarnette <mbdej1214@gmail.com>
2026-02-16 12:35:26 -0500

    before lunch

---
+++ src/week2/StringCalculatorKataJeffFriday/StringCalculator/Calculator.cs
@@ -0,0 +1,58 @@
+﻿
+// what 
+// "interface segregation principle"
+    // you define what you need, don't just use what's available
+
+public interface ILogger
+{
+    void LogAddResults(string results);
+}
+
+public interface INotifyTheHelpDesk
+{
+    void Notify(string v);
+}
+
+public class Calculator(ILogger _logger, INotifyTheHelpDesk _helpDesk)
+{
+    public int Add(string numbers)
+    {
+
+
+        var result = numbers == "" ? 0 : numbers // "1,2,3,4"
+            .Split(',', '\n') // ["1", "2", "3", "4"]
+            .Select(int.Parse) // [1, 2, 3, 4]
+            .Sum(); // 10
+
+        // Do something here
+        // this is a "non-functional" or "technical" requirement
+        // side effect just means that something is happening
+        // that doesn't change the observable behavior of the
+        // caller of this method
+        // 
+        // logging is "leaving the escape room" - leaving the process
+        // writing to the file system
+
+        try
+        {
+            _logger.LogAddResults(result.ToString());
+        }
+        catch (Exception)
+        {
+            // the help desk is notified
+            _helpDesk.Notify("Wasn't able to log: " + result.ToString());
+            // gulp
+        }
+        return result;
+
+    }
+}
+
+
+
+// test double
+// dummy - not really part of the test, just need something so we don't get a NRE
+// stub - a thing that has canned responses to questions. simulating faults
+// mock - record their interactions.
+// fake - we will do this in our api. it's not our code, it's a "stand in" for something
+// that will be there in "production" - 
---
+++ src/week2/StringCalculatorKataJeffFriday/StringCalculator/CalculatorInteractionTests.cs
@@ -0,0 +1,68 @@
+﻿
+
+using Castle.Core.Logging;
+using System.Net.Security;
+
+namespace StringCalculator;
+
+public class CalculatorInteractionTests
+{
+    [Theory]
+    [InlineData("1,2", "3")]
+    [InlineData("42", "42")]
+    public void TheLoggerLogs(string example, string expected)
+    {
+        // Given
+        var mockedLogger = Substitute.For<ILogger>();
+        var calculator = new Calculator(mockedLogger, Substitute.For<INotifyTheHelpDesk>());
+
+        // when
+        var result = calculator.Add(example);
+
+        // this test will only pass if
+        // "3" is written to the logger.
+        // then - this is the part that is going to pass or fail.
+        mockedLogger.Received().LogAddResults(expected);
+    }
+
+    [Theory]
+    [InlineData("1,2", "3")]
+    public void LoggerThrows(string example, string expected)
+    {
+        // Given
+        var stubbedLogger = Substitute.For<ILogger>();
+        var mockedNotifier = Substitute.For<INotifyTheHelpDesk>();
+        var calculator = new Calculator(stubbedLogger, mockedNotifier);
+        stubbedLogger.When(logger => logger.LogAddResults(Arg.Any<string>()))
+            .Throw(new Exception("Blammo~"));
+
+        // when
+        var result = calculator.Add(example);
+
+        // this test will only pass if
+        // "3" is written to the logger.
+        // then - this is the part that is going to pass or fail.
+        mockedNotifier.Received(1).Notify("Wasn't able to log: 3");
+    }
+
+    [Theory]
+    [InlineData("1,2", "3")]
+    public void HelpDeskNotnotifiedWithNoException(string example, string expected)
+    {
+        // Given
+        var stubbedLogger = Substitute.For<ILogger>();
+        var mockedNotifier = Substitute.For<INotifyTheHelpDesk>();
+        var calculator = new Calculator(stubbedLogger, mockedNotifier);
+        //stubbedLogger.When(logger => logger.LogAddResults(Arg.Any<string>()))
+        //    .Throw(new Exception("Blammo~"));
+
+        // when
+        var result = calculator.Add(example);
+
+        // this test will only pass if
+        // "3" is written to the logger.
+        // then - this is the part that is going to pass or fail.
+        mockedNotifier.DidNotReceive().Notify(Arg.Any<string>());
+    }
+}
+
