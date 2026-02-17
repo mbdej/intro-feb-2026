350382a8526c1147c392b3bfe711225268b28d28
Martin DeJarnette <mbdej1214@gmail.com>
2026-02-17 16:30:18 -0500

    copying jeff's code

--- src/week2/angular-demo/src/app/app.ts
+++ src/week2/angular-demo/src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from './nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Nav, RouterOutlet],
   template: `
-    <p>This is the story of my life</p>
-    <h2>Chapter 1 - in the beginning</h2>
+    <app-nav>
+      <main class="container mx-auto pt-4">
+        <router-outlet></router-outlet>
+      </main>
+    </app-nav>
   `,
   styles: [],
 })
-export class App {
-  protected readonly title = signal('angular-demo');
-}
+export class App {}
